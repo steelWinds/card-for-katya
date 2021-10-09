bb80b565d64a9959eb7ef8e49688c2eb8fea1e1e
ninja <kirillsurov0@gmail.com>
2021-10-10 04:48:54 +0900

    update: update all

--- src/assets/js/other/removeSelectors.js
+++
@@ -1,9 +0,0 @@
-let removeSelectors = (selector) => {
-    let elements = Array.from(document.querySelectorAll(selector));
-
-    elements.forEach((element) => {
-        element.remove();
-    });
-};
-
-export { removeSelectors };
